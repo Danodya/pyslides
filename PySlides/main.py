3f5f376da06e0dbeb9d9b73bb7d2e42e53f94b88
dw23411 <dw23411@essex.ac.uk>
2024-06-09 16:26:17 +0000

    square inside the new window created

--- PySlides/main.py
+++ PySlides/main.py
@@ -7,13 +7,22 @@ SCREEN_WIDTH = 794
 SCREEN_HIGHT = 1123
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HIGHT))
 
+# create a square
+player = pygame.Rect((300, 250, 50, 50))
+
 run = True
 # game loop
 while run:
+
+    # draw the square
+    pygame.draw.rect(screen, (255, 0, 0), player)
 
     # event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    # update window to display the square
+    pygame.display.update()
 
 pygame.quit()
