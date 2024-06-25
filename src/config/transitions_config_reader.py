f40aacaa3c4619c9760aad3a9a9bb01e1ff64799
dw23411 <dw23411@essex.ac.uk>
2024-06-26 00:29:42 +0100

    configuration json structure changed

--- src/config/transitions_config_reader.py
+++ src/config/transitions_config_reader.py
@@ -1,15 +1,20 @@
 import json
 
 class TransitionsConfig:
-
+    
     # Load the transitions configuration file
-    def load_transitions_config():
+    @staticmethod
+    def load_transitions_config(pdf_name):
         # Path to the transitions configuration file
-        config_path = '/Users/danodyaweerasinghe/Documents/MSc/Dissertation/22-24_CE901-CE911-CF981-SU_weerasinghe_danodya/src/config/transitions_config.json'
+        config_path = '/home/danodya/Documents/22-24_CE901-CE911-CF981-SU_weerasinghe_danodya/src/config/transitions_config.json'
         with open(config_path, 'r') as config_file:
             config = json.load(config_file)
-        return {item["slide"]: item["transition"] for item in config["demo"]}
+        
+        # Load transitions for the specified PDF
+        pdf_config = config.get(pdf_name, {})
+        return {int(key.split()[1]): {"transition": value["transition"], "duration": value["transition-duration"]} for key, value in pdf_config.items()}
 
-    # Function to get the transition type for a given slide
-    def get_transition_type(slide_transitions, slide_number):
-        return slide_transitions.get(slide_number, 'fade_in')  # Default transition is 'fade_in'
+    #Function to get the transition type and duration for a given slide
+    @staticmethod
+    def get_transition_config(slide_transitions, slide_number):
+        return slide_transitions.get(slide_number, {"transition": "fade_in", "duration": "1s"})  # Default transition and duration
