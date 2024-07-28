bb24906d338aa6fbde0b4ebe61855973e91cd764
danodya <danoweera95@gmail.com>
2024-07-28 21:56:02 +0100

    remove existing main.py file

--- pyslides/config/transitions_config_reader.py
+++ pyslides/config/transitions_config_reader.py
@@ -3,24 +3,44 @@ import pyslides.constant as constant
 
 
 class TransitionsConfig:
+
+    general_settings = {}
     
     # Load the transitions configuration file
     @staticmethod
     def load_transitions_config(config_file_name):
         # Path to the transitions configuration file
-        config_path = f'/pyslides/config/{config_file_name}'
+        config_path = f'pyslides/config/{config_file_name}'
         with open(config_path, 'r') as config_file:
             config = json.load(config_file)
-        
-        # Load transitions for the slides in the provided configuration
-        return {int(key.split()[1]): {"transition": value["transition"], "duration": value["transition-duration"],
-                                      "reversal-strategy": value["reversal-strategy"]} for key, value in config.items()}
+
+        # Extract general settings
+        TransitionsConfig.general_settings = config.get("General", {
+            "transition": "fade_in",
+            "transition-duration": "1s",
+            "reversal-strategy": "invert-transition"
+        })
+
+        # Load transitions for each slide and apply general settings if specific settings are missing
+        slide_transitions = {}
+        for key, value in config.items():
+            if key.startswith("Slide"):
+                slide_number = int(key.split()[1])
+                slide_transitions[slide_number] = {
+                    "transition": value.get("transition", TransitionsConfig.general_settings["transition"]),
+                    "duration": value.get("transition-duration", TransitionsConfig.general_settings["transition-duration"]),
+                    "reversal-strategy": value.get("reversal-strategy", TransitionsConfig.general_settings["reversal-strategy"])
+                }
+        # breakpoint()
+        return slide_transitions
 
     # Function to get the transition type and duration for a given slide
     @staticmethod
     def get_transition_config(slide_transitions, slide_number):
         return slide_transitions.get(slide_number,
-                                     {"transition": "fade_in", "duration": "1s", "reversal-strategy": "keep-original"})
+                                     {"transition": TransitionsConfig.general_settings["transition"],
+                                      "duration": TransitionsConfig.general_settings["transition-duration"],
+                                      "reversal-strategy": TransitionsConfig.general_settings["reversal-strategy"]})
         # Default transition, duration and reverse strategy
 
     # Function to check the reversal strategy of a given slide
